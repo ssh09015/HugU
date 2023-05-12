d31ea55bf8a7b3546d29ddc9f318287485e01784
ksyi <ssh09015_@naver.com>
2023-05-13 01:28:18 +0900

    도움 페이지 탭뷰, 뷰페이저 작업까지 완료

--- app/src/main/java/com/example/hugu/fragment/HelpFragment.kt
+++ app/src/main/java/com/example/hugu/fragment/HelpFragment.kt
@@ -1,21 +1,50 @@
 package com.example.hugu.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBindings
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.example.hugu.adapter.PagerFragmentStateAdapter
 import com.example.hugu.databinding.FragmentHelpBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class HelpFragment : Fragment() {
     lateinit var viewbinding: FragmentHelpBinding
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         viewbinding = FragmentHelpBinding.inflate(inflater, container, false)
-
         return viewbinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val pagerAdapter = PagerFragmentStateAdapter(requireActivity())
+
+        pagerAdapter.addFragment(HelpRankFragment())
+        pagerAdapter.addFragment(HelpSupportFragment())
+
+        viewbinding.viewPager.adapter=pagerAdapter
+
+        viewbinding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                Log.e("HelpFragment", "Page ${position+1}")
+            }
+        })
+
+        TabLayoutMediator(viewbinding.tabLayout, viewbinding.viewPager) { tab, position ->
+            tab.text = "Tab ${position+1}"
+        }.attach()
     }
 }
